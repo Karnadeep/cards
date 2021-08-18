7468aa44c566ab7116a9a4b524a217a4e553bab2
Karnadeep Rana <karnadeepmrana@gmail.com>
2021-08-18 15:59:11 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
+import React, { useEffect, useState, Fragment } from 'react'
+import { Container } from 'semantic-ui-react'
+import "semantic-ui-css/semantic.min.css"
 import './App.css';
+import store from './store'
+import { Provider } from "react-redux";
+import PropTypes from 'prop-types'
+import Cards from './Cards'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { loadPosts } from './actions/posts'
+
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    loadPosts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Fragment>
+        <Container>
+          <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
+        </Container>
+      </Fragment>
+    </Provider>
   );
 }
 
-export default App;
+App.propTypes = {
+  loadPosts: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+
+})
+
+export default connect(mapStateToProps, { loadPosts })(App);
---
+++ src/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Cards = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+Cards.propTypes = {
+
+}
+
+export default Cards
+
